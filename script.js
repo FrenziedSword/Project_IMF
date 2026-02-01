2e6deab0982b74fc26d32a70d77c8bc217ec4ab1
Arpit Kumar <180837570+FrenziedSword@users.noreply.github.com>
2026-02-02 00:26:48 +0530

    This is the Access Denied Page in case the user puts in wrong info.. Though the user can redirect back to clearance page (login).

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function generateRealIP() {
+  return Array.from({ length: 4 }, () =>
+    Math.floor(Math.random() * 256)
+  ).join(".");
+}
+
 const form = document.getElementById("loginForm");
 const msg = document.getElementById("message");
 
@@ -14,9 +20,22 @@ form.addEventListener("submit", function(e){
         if(agent === "shadow" && key === "7391"){
             msg.textContent = "Access Granted ✔";
             msg.style.color = "#00ff9c";
+
+            setTimeout(()=>{
+                window.location.href = "access.html";
+            }, 600);
         }else{
+            const realIP = generateRealIP();
+
+            sessionStorage.setItem("real_ip", realIP);
+
             msg.textContent = "Access Denied ✖";
             msg.style.color = "red";
+
+            setTimeout(function(){
+                // use href so the denied page is in history (user can go back)
+                window.location.href = "access_denied.html";
+            }, 600);
         }
     },1500);
 });
